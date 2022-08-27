36b2348ce69167a7a3e336e2b7e2a189be8f042b
AkshayKappala <akshaykappala98@gmail.com>
2022-08-27 23:10:25 +0530

    Add Navbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import { Navbar, NavbarBrand } from "reactstrap";
 
 function App() {
   return (
     <>
+      <Navbar className="my-0" color="secondary" dark>
+        <NavbarBrand href="/">
+          ONE CL<i class="bi bi-stopwatch BrandIcon"></i>CK
+        </NavbarBrand>
+      </Navbar>
       <h1>Greetings!</h1>
     </>
   );
